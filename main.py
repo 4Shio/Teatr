904bb63544fb86325250dc4e9b684f437f027f82
4Shio <katyushink92@gmail.com>
2023-10-15 22:52:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,30 +23,27 @@ def start(message):
     btn1 = types.KeyboardButton("Список всех следующих спектаклей")
     markup.add(btn, btn1)
     bot.send_message(message.chat.id, text='Привет от бота', reply_markup=markup)
-
-
 url = 'https://mrteatr.ru/'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 now = datetime.now()
 time_str = soup.find(class_='item').find(class_='time').text
 title = soup.find(class_='item').find('a').text
-monthstr = string
-monthint = 0
-tabl = pandas.DataFrame(columns=)
+speki ={}
 spectacless = soup.find_all(class_='item')
 datasp = 0
 while True:
-    for i in spectacless:
+    speki.clear()
+    for i,el in enumerate(spectacless):
         try:
-            timesp = i.find(class_='time').text.split()
-            tit = i.find('a').text
+            timesp = el.find(class_='time').text.split()
+            tit = el.find('a').text
             day = list(timesp)[0] + join(list(timesp)[1])
-            tabl = ({'day': [timesp[0]], 'month': [timesp[1]],  'Spec': [tit]})
-            print(tabl)
+            speki[i]=[timesp[0],timesp[1],timesp[3],tit]
         except:
             pass
-    time.sleep(60)
+
+    time.sleep(600)
 # 19 октября среда 18:00
 
 @bot.message_handler(content_types=['text'])
@@ -68,7 +65,7 @@ def func(message):
         a = telebot.types.ReplyKeyboardRemove()
         bot.send_message(message.from_user.id, 'Выберите нужный вам спектакль', reply_markup=a)
         @bot.message_handler(content_types=['text'])
-        def oprsp(message):
-            bot.send_message(message.chat.id,text=
-            tabl.loc[tabl['Spec']== message.text])
+        def maese(message):
+
+
 bot.infinity_polling()
