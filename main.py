2c18f058ccde969b2f2dc06a32884b0b891b959f
4Shio <katyushink92@gmail.com>
2023-10-15 22:19:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from datetime import *
 import time
 import re
 import pandas
+
 now = datetime.now()
 id = [551057845, 1080107997, 6386209825, 402783140]
 bot = telebot.TeleBot('6426552218:AAEAcGWJ69_D3lZB_Ln6v5GRZlULOUR-3V0')
@@ -22,6 +23,8 @@ def start(message):
     btn1 = types.KeyboardButton("Список всех следующих спектаклей")
     markup.add(btn, btn1)
     bot.send_message(message.chat.id, text='Привет от бота', reply_markup=markup)
+
+
 url = 'https://mrteatr.ru/'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
@@ -30,42 +33,21 @@ time_str = soup.find(class_='item').find(class_='time').text
 title = soup.find(class_='item').find('a').text
 monthstr = string
 monthint = 0
-tabl = pandas.DataFrame
+tabl = pandas.DataFrame(columns=)
 spectacless = soup.find_all(class_='item')
-datasp =0
+datasp = 0
 while True:
-    for spectaclel in spectacless:
-        
-        timesp = spectaclel.find(class_='time').text
-        tit = spectaclel.find('a').text
-        if list(timesp)[3] =='о'or list(timesp)[2] == 'о':
-            monthstr = 'Октябрь'
-            monthint = 10
-        if list(timesp)[3]=='н' or list(timesp)[2]=='н' and list(timesp)[4]=='о':
-            monthstr= 'Ноябрь'
-            monthint = 11
-        if list(timesp)[3] =='д'or list(timesp)[2] =='д':
-            monthstr = 'Декабрь'
-            monthint = 12
-        if list(timesp)[3]=='я'or list(timesp)[2]=='я':
-            monthstr= 'Январь'
-            monthint = 1
-        if list(timesp)[3] =='ф'or list(timesp)[2] == 'ф':
-            monthstr = 'Февраль'
-            monthint = 2
-        if list(timesp)[3]=='м' or list(timesp)[2]=='н' and (list(timesp)[5]=='р'or list(timesp)[4]=='р'):
-            monthstr= 'Март'
-            monthint = 3
-        if list(timesp)[3] =='а'or list(timesp)[2] =='а':
-            monthstr = 'Апрель'
-            monthint = 4
-        if list(timesp)[3]=='м' or list(timesp)[2]=='н' and (list(timesp)[5]=='й'or list(timesp)[4]=='й'):
-            monthstr= 'Май'
-            monthint = 5
-        day = list(timesp)[0] + join(list(timesp)[1])
-        tabl = ({'day': [day],'month':[monthstr],'monthi':[monthint], 'Spec': [tit]})
-        print(tabl)
+    for i in spectacless:
+        try:
+            timesp = i.find(class_='time').text.split()
+            tit = i.find('a').text
+            day = list(timesp)[0] + join(list(timesp)[1])
+            tabl = ({'day': [timesp[0]], 'month': [timesp[1]],  'Spec': [tit]})
+            print(tabl)
+        except:
+            pass
     time.sleep(60)
+# 19 октября среда 18:00
 
 @bot.message_handler(content_types=['text'])
 def func(message):
@@ -73,12 +55,20 @@ def func(message):
         time_str = soup.find(class_='item').find(class_='time').text
         title = soup.find(class_='item').find('a').text
         bot.send_message(message.chat.id, text=f"{time_str}\n{title}")
+
     if (message.text == "Список всех следующих спектаклей"):
         spectacles = soup.find_all(class_='item')
         for spectacle in spectacles:
-            time = spectacle.find(class_='time').text
-            titles = spectacle.find('a').text
-            bot.send_message(message.chat.id, text=f"{time}\n{titles}")
-
-
+            try:
+                time = spectacle.find(class_='time').text
+                titles = spectacle.find('a').text
+                bot.send_message(message.chat.id, text=f"{time}\n{titles}")
+            except:pass
+    if (message.text== "Найти определённый спектакль"):
+        a = telebot.types.ReplyKeyboardRemove()
+        bot.send_message(message.from_user.id, 'Выберите нужный вам спектакль', reply_markup=a)
+        @bot.message_handler(content_types=['text'])
+        def oprsp(message):
+            bot.send_message(message.chat.id,text=
+            tabl.loc[tabl['Spec']== message.text])
 bot.infinity_polling()
