7664ae2b245ed0adcc5d0f4b39929c6ad2044ef0
4Shio <katyushink92@gmail.com>
2023-10-15 20:38:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ def start(message):
     btn1 = types.KeyboardButton("Список всех следующих спектаклей")
     markup.add(btn, btn1)
     bot.send_message(message.chat.id, text='Привет от бота', reply_markup=markup)
-
-
 url = 'https://mrteatr.ru/'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
@@ -35,25 +33,39 @@ monthint = 0
 tabl = pandas.DataFrame
 spectacless = soup.find_all(class_='item')
 datasp =0
-for spectaclel in spectacless:
-    timesp = spectaclel.find(class_='time').text
-    tit = spectaclel.find('a').text
-    if list(timesp)[3] =='о'or list(timesp)[2] == 'о':
-        monthstr = 'Октябрь'
-        monthint = 10
-    if list(timesp)[3]=='н' or list(timesp)[2]=='н' and list(timesp)[4]=='о':
-        monthstr= 'Ноябрь'
-        monthint = 11
-    if list(timesp)[3] =='д'or list(timesp)[2] =='д':
-        monthstr = 'Декабрь'
-        monthint = 12
-    if list(timesp)[3]=='я'or list(timesp)[2]=='я':
-        monthstr= 'Январь'
-        monthint = 1
-    day = list(timesp)[0] + join(list(timesp)[1])
-    tabl = ({'day': [day],'month':[monthstr],'monthi':[monthint], 'Spec': [tit]})
-    print(tabl)
-
+while True:
+    for spectaclel in spectacless:
+        
+        timesp = spectaclel.find(class_='time').text
+        tit = spectaclel.find('a').text
+        if list(timesp)[3] =='о'or list(timesp)[2] == 'о':
+            monthstr = 'Октябрь'
+            monthint = 10
+        if list(timesp)[3]=='н' or list(timesp)[2]=='н' and list(timesp)[4]=='о':
+            monthstr= 'Ноябрь'
+            monthint = 11
+        if list(timesp)[3] =='д'or list(timesp)[2] =='д':
+            monthstr = 'Декабрь'
+            monthint = 12
+        if list(timesp)[3]=='я'or list(timesp)[2]=='я':
+            monthstr= 'Январь'
+            monthint = 1
+        if list(timesp)[3] =='ф'or list(timesp)[2] == 'ф':
+            monthstr = 'Февраль'
+            monthint = 2
+        if list(timesp)[3]=='м' or list(timesp)[2]=='н' and (list(timesp)[5]=='р'or list(timesp)[4]=='р'):
+            monthstr= 'Март'
+            monthint = 3
+        if list(timesp)[3] =='а'or list(timesp)[2] =='а':
+            monthstr = 'Апрель'
+            monthint = 4
+        if list(timesp)[3]=='м' or list(timesp)[2]=='н' and (list(timesp)[5]=='й'or list(timesp)[4]=='й'):
+            monthstr= 'Май'
+            monthint = 5
+        day = list(timesp)[0] + join(list(timesp)[1])
+        tabl = ({'day': [day],'month':[monthstr],'monthi':[monthint], 'Spec': [tit]})
+        print(tabl)
+    time.sleep(60)
 
 @bot.message_handler(content_types=['text'])
 def func(message):
