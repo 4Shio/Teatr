f33a5bdd0ce889d56a53d071e2148308370f0a37
4Shio <katyushink92@gmail.com>
2023-10-18 20:01:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import datetime
-import string
-from shlex import join
 import telebot
 import bs4
 from bs4 import BeautifulSoup
@@ -10,6 +8,7 @@ from datetime import *
 import time
 import re
 import pandas
+import asyncio
 
 now = datetime.now()
 id = [551057845, 1080107997, 6386209825, 402783140]
@@ -23,49 +22,42 @@ def start(message):
     btn1 = types.KeyboardButton("Список всех следующих спектаклей")
     markup.add(btn, btn1)
     bot.send_message(message.chat.id, text='Привет от бота', reply_markup=markup)
+
+
 url = 'https://mrteatr.ru/'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 now = datetime.now()
 time_str = soup.find(class_='item').find(class_='time').text
 title = soup.find(class_='item').find('a').text
-speki ={}
+speki = {}
 spectacless = soup.find_all(class_='item')
-datasp = 0
-while True:
-    speki.clear()
-    for i,el in enumerate(spectacless):
-        try:
-            timesp = el.find(class_='time').text.split()
-            tit = el.find('a').text
-            day = list(timesp)[0] + join(list(timesp)[1])
-            speki[i]=[timesp[0],timesp[1],timesp[3],tit]
-        except:
-            pass
+months = {'октября': 10, 'ноября': 11, 'декабря': 12, 'января': 1, 'февраля': 2, 'марта': 3, 'апреля': 4, 'мая': 5,
+          'июня': 6, 'июля': 7, 'сентября': 9}
+for i, el in enumerate(spectacless):
+    try:
+        timesp = el.find(class_='time').text.split()
+        tit = el.find('a').text
+        speki[i] = [timesp[0], timesp[1], months[timesp[1]], timesp[3], tit]
+    except:
+        pass
 
-    time.sleep(600)
 # 19 октября среда 18:00
 
 @bot.message_handler(content_types=['text'])
 def func(message):
     if (message.text == "Следующий спектакль"):
-        time_str = soup.find(class_='item').find(class_='time').text
-        title = soup.find(class_='item').find('a').text
-        bot.send_message(message.chat.id, text=f"{time_str}\n{title}")
+        if datetime([0][0]) == now.day - timedelta(days=1) and datetime(speki[0][2]) == now.month:
+            bot.send_message(message.chat.id,text=f'Завта в {speki[0][0]} будет {speki[0][4]}\n Приходить к {speki[0][0]-timedelta(hours=1)}')
+        bot.send_message(message.chat.id, text=f"{speki[0]}")
 
     if (message.text == "Список всех следующих спектаклей"):
-        spectacles = soup.find_all(class_='item')
-        for spectacle in spectacles:
-            try:
-                time = spectacle.find(class_='time').text
-                titles = spectacle.find('a').text
-                bot.send_message(message.chat.id, text=f"{time}\n{titles}")
-            except:pass
-    if (message.text== "Найти определённый спектакль"):
+        for i in speki:
+            bot.send_message(message.chat.id,text=f'{speki[i]}\n')
+    if (message.text == "Найти определённый спектакль"):
         a = telebot.types.ReplyKeyboardRemove()
         bot.send_message(message.from_user.id, 'Выберите нужный вам спектакль', reply_markup=a)
-        @bot.message_handler(content_types=['text'])
-        def maese(message):
+
 
 
 bot.infinity_polling()
