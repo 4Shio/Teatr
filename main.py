747e66553fa0b011eaf960c48a10082d131d145f
4Shio <katyushink92@gmail.com>
2023-11-14 16:41:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,69 @@
 import datetime
+import time
 
 import bs4
 from bs4 import BeautifulSoup
 import requests
-
 from datetime import *
 import asyncio
 import aiogram
-from aiogram import*
-from aiogram.filters.command import Command
+from aiogram import *
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+import numpy as np
+
 now = datetime.now()
 id = [551057845, 1080107997, 6386209825, 402783140]
 bot = Bot(token='6426552218:AAEAcGWJ69_D3lZB_Ln6v5GRZlULOUR-3V0')
-speki ={}
-dp = Dispatcher
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Привет от бота",reply_markup=markup)
-
-@dp.message('Следующий спектакль')
-async def comands(message:types.Message):
-    await message.answer("Да")
-
-async def main():
-    await dp.start_polling(bot)
-    await update()
+speki = {}
+dp = Dispatcher()
 
 
+
+async def command_start(message:Message) -> None:
+    await message.answer(f"Привет от бота ")
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    global formatted_text
+    if message.text == "Следующий спектакль":
+        await bot.send_message(chat_id=message.chat.id, text=f"{speki[0]}")
 async def update():
-    while True:
-        url = 'https://mrteatr.ru/'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.text, "html.parser")
-        now = datetime.now()
-        time_str = soup.find(class_='item').find(class_='time').text
-        title = soup.find(class_='item').find('a').text
-        spectacless = soup.find_all(class_='item')
-        months = {'октября': 10, 'ноября': 11, 'декабря': 12, 'января': 1, 'февраля': 2, 'марта': 3, 'апреля': 4, 'мая': 5,
+
+    url = 'https://mrteatr.ru/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    spectacless = soup.find_all(class_='item')
+    months = {'октября': 10, 'ноября': 11, 'декабря': 12, 'января': 1, 'февраля': 2, 'марта': 3, 'апреля': 4,
+                  'мая': 5,
                   'июня': 6, 'июля': 7, 'сентября': 9}
-        for i, el in enumerate(spectacless):
+    for i, el in enumerate(spectacless):
             try:
                 timesp = el.find(class_='time').text.split()
                 tit = el.find('a').text
                 speki[i] = [timesp[0], timesp[1], months[timesp[1]], timesp[3], tit]
             except:
                 pass
-        print(speki[0][2])
+    file = open('Base', 'w')
+    file.write()
+    file.close
+
+    #await asyncio.sleep(1000)
+async def main():
+    while True:
+        task1 = asyncio.create_task(update())
+        task2 = asyncio.create_task(dp.start_polling(bot))
+        await  task1
+        await task2
+
 
 
 
 
 if __name__ == "__main__":
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn = types.KeyboardButton("Следующий спектакль")
-    btn1 = types.KeyboardButton("Список всех следующих спектаклей")
-    markup.add(btn, btn1)
+
     asyncio.run(main())
-bot.infinity_polling()
+    print(speki[0])
