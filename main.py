e2513903db7646d66f0f043ef4922b41abed833b
4Shio <katyushink92@gmail.com>
2023-12-13 12:52:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,59 @@
 import datetime
 import time
 import bs4
+from aiogram.filters import Command
 from bs4 import BeautifulSoup
 import requests
 from datetime import *
 import asyncio
 import aiogram
 import sqlite3
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.types import Message
+from aiogram import *
+from aiogram.types import *
 
 now = datetime.now()
 bot = Bot(token='6426552218:AAEAcGWJ69_D3lZB_Ln6v5GRZlULOUR-3V0')
 speki = {}
-spek = {}
+# spek = {}
+# global spek =
 dp = Dispatcher()
+connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
 
-async def command_start(message: Message) -> None:
-    await message.answer(f"Привет от бота ")
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="Следующий спектакль")],
+        [types.KeyboardButton(text="Список всех следующих спектаклей")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    await bot.send_message(chat_id=message.chat.id, text="Привет от бота ", reply_markup=keyboard)
 
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
     if message.text == "Следующий спектакль":
-        spek = ' '.join(str(i) for i in speki[0])
-        await bot.send_message(chat_id=message.chat.id, text=f"{spek}")
+        cursor.execute("SELECT * FROM Spektakli")
+        await bot.send_message(chat_id=message.chat.id, text=' '.join(str(i) for i in cursor.fetchall()[0]))
     if message.text == "Список всех следующих спектаклей":
-        spekt = '\n'.join(' '.join(str(i) for i in v) for v in speki.values())
+        cursor.execute("SELECT * FROM Spektakli")
+        spekt = '\n'.join(' '.join(str(i) for i in v) for v in cursor.fetchall())
         await bot.send_message(chat_id=message.chat.id, text=f"{spekt}")
 
 
 async def update():
+    cursor.execute('''DROP TABLE IF EXISTS Spektakli ''')
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Spektakli (
+            day INTEGER,
+            month TEXT NOT NULL,
+            months INTEGER,
+            time INTEGER,
+            name TEXT NOT NULL,
+            vrem INTEGER
+            )
+            ''')
     url = 'https://mrteatr.ru/'
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
@@ -43,21 +66,14 @@ async def update():
             timesp = el.find(class_='time').text.split()
             tit = el.find('a').text
             speki[i] = [timesp[0], timesp[1], months[timesp[1]], timesp[3], tit]
+            cursor.execute('INSERT INTO Spektakli (day, month, months, time, name) VALUES (?, ?, ?, ?, ? )',
+                           (timesp[0], timesp[1], months[timesp[1]], timesp[3], tit))
+
         except:
             pass
+        cursor.execute("SELECT * FROM Spektakli")
 
-        connection = sqlite3.connect('my_database.db')
-        cursor = connection.cursor()
-        cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL,
-        age INTEGER
-        )
-        ''')
         connection.commit()
-        connection.close()
     await asyncio.sleep(10000)
 
 
