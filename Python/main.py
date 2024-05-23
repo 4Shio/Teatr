29bd954e1b926049e4f6cac9fa8babd3fed14398
4Shio <katyushink92@gmail.com>
2024-05-23 20:35:27 +0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -12,6 +12,7 @@ from sql import change_data, fetchall, fetchone, get_connection
 
 connection , cursor = get_connection()
 now = datetime.now()
+today = now.strftime("%d %m")
 bot = Bot(token='6426552218:AAEAcGWJ69_D3lZB_Ln6v5GRZlULOUR-3V0')
 
 symvols_to_delete = "/"
@@ -29,7 +30,7 @@ remove_key = ReplyKeyboardRemove()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Привет от бота", reply_markup=make_row_keyboard(["View", "Add","analys"] ))
+    await message.answer("Привет от бота", reply_markup=make_row_keyboard(["View", "LAfterT","analys"] ))
 
 @dp.message(F.text == "View")
 async def view(message: types.Message):
@@ -42,6 +43,20 @@ async def Del(message: types.Message):
 @dp.message(F.text == "analys")
 async def analys(message: types.Message):
     await message.answer(text='\n'.join(' '.join (str(i) for i in v ) for v in fetchall("SELECT name, turns FROM analys")))
+
+@dp.message(F.text == "LAfterT")
+async def LAfrerT(message: types.Message):
+    dates = fetchall("SELECT date FROM test")
+    Speki_after =[]
+    for i,e in enumerate (dates):
+        if (datetime.strptime(dates[i][0],'%d %m')) > datetime.strptime(today,"%d %m"):
+
+            Speki_after.append(fetchone("SELECT name , date FROM test WHERE date = %s",(dates[i])))
+            await message.answer(text=Speki_after)
+                                
+                         
+                        
+
 
 pages = ["https://mrteatr.ru/afisha/", "https://mrteatr.ru/afisha/?page=2", "https://mrteatr.ru/afisha/?page=3",
          "https://mrteatr.ru/afisha/?page=4"]
@@ -79,7 +94,7 @@ async def update():
                     if fetchone("SELECT COUNT(*) FROM test WHERE name =%s AND date = %s",(tit,datesp)) == 0:
                          
                         change_data("INSERT INTO TEST (date, name, time, info ) VALUES (%s ,%s ,%s, %s)", (datesp, tit, timesp, info))
-                    
+                   
                 except Exception as ex:
                     print(ex)
         except:
@@ -88,6 +103,18 @@ async def update():
 
 
     print('Update complete')
+    dates = fetchall("SELECT date FROM test")
+    for i,e in enumerate (dates):
+        if (datetime.strptime(dates[i][0],'%d %m')) > datetime.strptime(today,"%d %m"):
+            #print(dates[i][0])
+            print(fetchall("SELECT name ,date ,time FROM test WHERE date = %s",(dates[i])))
+
+           
+
+
+    
+    
+
     try:
         names = fetchall("SELECT name FROM test") 
         for i,e in enumerate (names):
@@ -103,12 +130,10 @@ async def update():
 
 
 async def main():
-    while True:
-        task1 = asyncio.create_task(update())
-        task2 = asyncio.create_task(dp.start_polling(bot))
-        await task1
-        await task2
-
+    task1 = asyncio.create_task(update())
+    task2 = asyncio.create_task(dp.start_polling(bot))
+    await task1
+    await task2
 
 if __name__ == "__main__":
     asyncio.run(main())
