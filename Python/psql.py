521f930a6431d50060b514e71c16d05c2f12ef6b
4Shio <katyushink92@gmail.com>
2024-05-31 16:31:40 +0300

    Update psql.py

--- Python/psql.py
+++ Python/psql.py
@@ -1,5 +1,8 @@
+
 import psycopg2
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,MetaData,Table,String,Integer,Column,DateTime,Boolean,Text
+import datetime
+now =datetime.datetime.now
 try:
     engine = create_engine('postgresql+psycopg2://admin:root@78.36.44.30:5432/test')
     engine.connect()
@@ -8,3 +11,14 @@ try:
 except Exception as ex:
     print(ex)
   
+metadata = MetaData()
+
+blog = Table('blog', metadata, 
+    Column('id', Integer(), primary_key=True),
+    Column('post_title', String(200), nullable=False),
+    Column('post_slug', String(200),  nullable=False),
+    Column('content', Text(), nullable=False),
+    Column('published', Boolean(), default=False),
+    Column('created_on', DateTime(), default=now),
+    Column('updated_on', DateTime(), default=now, onupdate=now)
+)
