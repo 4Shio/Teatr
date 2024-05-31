2fea1eec554158c252b1e515e97778bb444da126
4Shio <katyushink92@gmail.com>
2024-05-31 16:04:09 +0300

    Update psql.py

--- Python/psql.py
+++ Python/psql.py
@@ -1,3 +1,10 @@
 import psycopg2
-from sqlalchemy import URl,create_engine,text
-
+from sqlalchemy import create_engine
+try:
+    engine = create_engine('postgresql+psycopg2://admin:root@78.36.44.30:5432/test')
+    engine.connect()
+    print('Connection Success')
+   
+except Exception as ex:
+    print(ex)
+  
