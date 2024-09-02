aed90fe7da4c34fd7ce020b0df3bdaf914c49c21
4Shio <katyushink92@gmail.com>
2024-09-03 00:01:27 +0300

    Update base.py

--- base.py
+++ base.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column,relationship
+from sqlalchemy.orm import Mapped,mapped_column
 from config import Base
 from datetime import datetime
 
