456e72557109093a53719e9ef165da2ed0515176
4Shio <katyushink92@gmail.com>
2024-09-04 21:53:46 +0300

    Update mesager.py

--- mesager.py
+++ mesager.py
@@ -1,19 +1,22 @@
 from base import user,Speki
 from config import async_session
-
 from datetime import *
+from config import tg_token
+from aiogram import Bot
 from sqlalchemy import select,func
-
+bot = Bot(tg_token)
 
 async def mesager():
-    while True:
         try:
             async with async_session() as session:
-                stmt = select(Speki.date).order_by(Speki.date)
+                stmt = select(Speki.date).order_by(Speki.date).where(Speki.date > datetime.now())
                 first_date = await session.scalar(stmt)
                 time_untill = first_date  - datetime.now()
-                if datetime.now().day == (first_date - timedelta(days= 1)):
-                    print()
+                if time_untill == timedelta(days=1,hours=0):
+                    print('Work')
+                    stmts = select(user.id)
+                    id_tg = await session.scalar(stmts)
+                    bot.send_message(chat_id=id_tg ,text='Time to work')
         except Exception as ex:
             print(ex)
             
