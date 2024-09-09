a2521089ae4c604a5e419ba8afa9b634c6acb467
4Shio <katyushink92@gmail.com>
2024-09-09 20:32:18 +0300

    Update mesager.py

--- mesager.py
+++ mesager.py
@@ -4,6 +4,7 @@ from datetime import *
 from config import tg_token
 from aiogram import Bot
 from sqlalchemy import select,func,update
+from handler import make_more_str
 bot = Bot(tg_token)
 
 async def mesager():
@@ -12,34 +13,41 @@ async def mesager():
             async with async_session() as session:
                 stmt = select(Speki.date).order_by(Speki.date).where(Speki.date > datetime.now())
                 first_date = await session.scalar(stmt)
-                time_untill = first_date  - datetime.now()
-               
-                if time_untill == timedelta(days=1,hours=0):
-                    
-                    print('Work')
-                    print((await session.execute(select(user.note))).all())
+                next_one = await   session.scalar(select(Speki.message_text).filter(Speki.date > datetime.now()).order_by(Speki.date))
+                
+                if (datetime.now() + timedelta(days=1)).date == first_date.date and datetime.now().hour == (first_date.hour - timedelta(hours=2)) :
                     stmts = select(user.t_id).where(user.note == True)
                     id_tg = (await session.execute(stmts)).scalars()
+                      
                     for i in id_tg:
                        print(i)
-                       await bot.send_message(chat_id=i ,text='Time to work')
+                       await bot.send_message(chat_id=i ,text = next_one)
                     
-                        
-                        
-                        
-                        
+                    pass
+                if datetime.now().date == first_date.date and datetime.now().time == first_date.time - timedelta(hours=2):
+                    stmts = select(user.t_id).where(user.note == True)
+                    id_tg = (await session.execute(stmts)).scalars()
+                      
+                    for i in id_tg:
+                       print(i)
+                       await bot.send_message(chat_id=i ,text = next_one)
+                    
+                    pass
+                
+                
                         
                 if datetime.now().weekday() == 0:
                     
-                    #stmts = select(Speki.name,Speki.date).where(Speki.date > datetime.now() and Speki.date< datetime.now() + timedelta(days=7))
-                    #next_week = await session.scalars(stmts)
+                    next_week_speki = select(Speki.message_text).filter(Speki.date > datetime.now()).filter(Speki.date <= (datetime.now() + timedelta(days=6))).order_by(Speki.date)
+                    next_week = (await session.execute(next_week_speki)).all()
+                    
                     users = (await session.execute(select(user.t_id).where(user.note == True))).scalars()
                     
-                    for i in (await session.execute(select(user.note))).scalars():
-                        print(i)
+             
+                        
                     for i in users:
-                        await bot.send_message(chat_id=i,text = 'Full week work')
-                    
+                        await bot.send_message(chat_id=i,text =make_more_str(next_week))
+
                     pass
             await session.close()
         except Exception as ex:
