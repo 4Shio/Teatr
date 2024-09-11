0a25da21635915317af7917deb4715eab471aa3e
4Shio <katyushink92@gmail.com>
2024-09-11 17:26:26 +0300

    Update mesager.py

--- mesager.py
+++ mesager.py
@@ -1,4 +1,4 @@
-from base import user,Speki
+from base import user,Speki,notes
 from config import async_session
 from datetime import *
 from config import tg_token
@@ -7,9 +7,12 @@ from sqlalchemy import select,func,update
 from handler import make_more_str
 bot = Bot(tg_token)
 
+
+
 async def mesager():
-   # while True:
+    while True:
         try:
+            
             async with async_session() as session:
                 stmt = select(Speki.date).order_by(Speki.date).where(Speki.date > datetime.now())
                 first_date = await session.scalar(stmt)
@@ -23,8 +26,8 @@ async def mesager():
                        print(i)
                        await bot.send_message(chat_id=i ,text = next_one)
                     
-                    pass
-                if datetime.now().date == first_date.date and datetime.now().time == first_date.time - timedelta(hours=2):
+                    
+                if datetime.now().date == first_date.date and datetime.now().hour == first_date.hour - timedelta(hours=2):
                     stmts = select(user.t_id).where(user.note == True)
                     id_tg = (await session.execute(stmts)).scalars()
                       
@@ -32,21 +35,23 @@ async def mesager():
                        print(i)
                        await bot.send_message(chat_id=i ,text = next_one)
                     
-                    pass
+                    
                 
                 
                         
                 if datetime.now().weekday() == 0:
                     
-                    next_week_speki = select(Speki.message_text).filter(Speki.date > datetime.now()).filter(Speki.date <= (datetime.now() + timedelta(days=6))).order_by(Speki.date)
-                    next_week = (await session.execute(next_week_speki)).all()
-                    
-                    users = (await session.execute(select(user.t_id).where(user.note == True))).scalars()
-                    
-             
-                        
-                    for i in users:
-                        await bot.send_message(chat_id=i,text =make_more_str(next_week))
+                    if monday_ckeck != datetime.now().date or monday_ckeck == None:
+                        monday_ckeck = datetime.now().date
+                        next_week_speki = select(Speki.message_text).filter(Speki.date > datetime.now()).filter(Speki.date <= (datetime.now() + timedelta(days=6))).order_by(Speki.date)
+                        next_week = (await session.execute(next_week_speki)).all()
+
+                        users = (await session.execute(select(user.t_id).where(user.note == True))).scalars()
+                        monday_ckeck = datetime.now().date
+
+
+                        for i in users:
+                            await bot.send_message(chat_id=i,text =make_more_str(next_week))
 
                     pass
             await session.close()
