78aaf0c17c6fbf8dd5f35e7404f12ab6f8de0d90
4Shio <katyushink92@gmail.com>
2024-09-16 17:10:01 +0300

    F

--- mesager.py
+++ mesager.py
@@ -3,7 +3,7 @@ from config import async_session
 from datetime import *
 from config import tg_token
 from aiogram import Bot
-from sqlalchemy import select,func,update
+from sqlalchemy import select,func,update,desc
 from handler import make_more_str
 import asyncio
 bot = Bot(tg_token)
@@ -16,24 +16,24 @@ async def mesager():
         try:
             
             async with async_session() as session:
+                
                 stmt = select(Speki.date).order_by(Speki.date).where(Speki.date > datetime.now())
                 first_date = await session.scalar(stmt)
-                next_one = await   session.scalar(select(Speki.message_text).filter(Speki.date > datetime.now()).order_by(Speki.date))
-                
-                if (datetime.now() + timedelta(days=1)).date == first_date.date and datetime.now().hour == (first_date.hour - timedelta(hours=2)) :
-                    stmts = select(user.t_id).where(user.note == True)
-                    id_tg = (await session.execute(stmts)).scalars()
-                      
-                    for i in id_tg:
+                next_one = await session.scalar(select(Speki.message_text).filter(Speki.date > datetime.now()).order_by(Speki.date))
+                users = (await session.execute(select(user.t_id).where(user.note == True))).scalars()
+               
+                if (datetime.now() + timedelta(days=1)).date() == first_date.date() and datetime.now().hour == 12 :
+                                          
+                    for i in users:
                        print(i)
                        await bot.send_message(chat_id=i ,text = next_one)
                     
                     
-                if datetime.now().date == first_date.date and datetime.now().hour == first_date.hour - timedelta(hours=2):
-                    stmts = select(user.t_id).where(user.note == True)
-                    id_tg = (await session.execute(stmts)).scalars()
-                      
-                    for i in id_tg:
+                    
+                    
+                if datetime(datetime.now().strftime("%Y-%m-%d %H")) == (datetime(first_date.strftime("%Y-%m-%d %H")) - timedelta(hours=2)):
+                   
+                    for i in users:
                        print(i)
                        await bot.send_message(chat_id=i ,text = next_one)
                     
@@ -43,33 +43,24 @@ async def mesager():
                         
                 if datetime.now().weekday() == 0:
                     
-                    last_update = await session.scalar(select(notes.date).filter(notes.type == 'week').order_by(notes.date.desc))
-                    
-                        
-                        
-                    if last_update.date != datetime.now().date or last_update == None:
-                        last_up = notes(date = datetime.now(),type = 'week')
-                        await session.add(last_up)
-                        await session.commit()
-                        
-                        
+                    last_update = await session.scalar(select(notes.date).filter(notes.type == 'week').order_by(desc(notes.date)))
+
+                    if  last_update == None or last_update.date() < datetime.now().date():
                         
                         next_week_speki = select(Speki.message_text).filter(Speki.date > datetime.now()).filter(Speki.date <= (datetime.now() + timedelta(days=6))).order_by(Speki.date)
                         next_week = (await session.execute(next_week_speki)).all()
-
-                        users = (await session.execute(select(user.t_id).where(user.note == True))).scalars()
                         
-
-
                         for i in users:
                             await bot.send_message(chat_id=i,text =make_more_str(next_week))
 
-                    pass
+                        last_up = notes(date = datetime.now(), type = 'week')
+                        session.add(last_up)
+                        await session.commit()
+                    
             await session.close()
         except Exception as ex:
-            await asyncio.sleep(100)
             print(ex)
-            
+            await asyncio.sleep(100)
             
             
             
